d606ee978be4092fc9757a7e5af6c99363cd405b
Denis Shtabnoy <tr0ythex@gmail.com>
2016-03-09 13:47:36 +0000

    Adds fake data products

--- js/angularjs/controllers/genericControllers.js
+++ js/angularjs/controllers/genericControllers.js
@@ -17,6 +17,22 @@ angular.module("tomaTort")
     $window.location.reload();
   };
 })
+.controller("tomaTortCtrl", function ($scope, $http) {
+  $scope.data = {
+    products: [
+      {"name": "Торт Наполеон", "price": "1500", "weight": "500"},
+      {"name": "Желе", "price": "1515", "weight": "500"},
+      {"name": "Пастила", "price": "1099", "weight": "400"},
+      {"name": "Пончик с повидлом", "price": "1560", "weight": "400"},
+      {"name": "Эклер Вкусный", "price": "1500", "weight": "700"},
+      {"name": "Бельгийские фавли", "price": "1099", "weight": "800"},
+      {"name": "Суфле Суфле", "price": "1500", "weight": "500"},
+      {"name": "Пирожное Наслаждение", "price": "1500", "weight": "400"},
+      {"name": "Мармелад Домашний", "price": "1199", "weight": "700"},
+      {"name": "Халва", "price": "1199", "weight": "600"}
+    ]
+  };
+})
 .directive("gallerySlider", function() {
 	return {
 		restrict: 'A',
