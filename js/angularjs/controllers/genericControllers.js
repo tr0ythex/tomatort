1883088573a277359faeae467ab705098a18f77c
Denis Shtabnoy <tr0ythex@gmail.com>
2016-03-10 21:32:41 +0000

    Adds product imageUrl property and upload some fake images for products

--- js/angularjs/controllers/genericControllers.js
+++ js/angularjs/controllers/genericControllers.js
@@ -29,16 +29,26 @@ angular.module("tomaTort")
 .controller("tomaTortCtrl", function ($scope, $location, menuActiveClass, cart) {
   $scope.data = {
     products: [
-      {"id": 1, "name": "Торт Наполеон", "price": 1500, "weight": 500},
-      {"id": 2, "name": "Желе", "price": 1515, "weight": 500},
-      {"id": 3, "name": "Пастила", "price": 1099, "weight": 400},
-      {"id": 4, "name": "Пончик с повидлом", "price": 1560, "weight": 400},
-      {"id": 5, "name": "Эклер Вкусный", "price": 1500, "weight": 700},
-      {"id": 6, "name": "Бельгийские фавли", "price": 1099, "weight": 800},
-      {"id": 7, "name": "Суфле Суфле", "price": 1500, "weight": 500},
-      {"id": 8, "name": "Пирожное Наслаждение", "price": 1500, "weight": 400},
-      {"id": 9, "name": "Мармелад Домашний", "price": 1199, "weight": 700},
-      {"id": 10, "name": "Халва", "price": 1199, "weight": 600}
+      {"id": 1, "name": "Торт Наполеон", "price": 1500, "weight": 500, 
+       "imageUrl": "/images/products/napoleon.jpg"},
+      {"id": 2, "name": "Желе", "price": 1515, "weight": 500, 
+       "imageUrl": "/images/products/zhele.jpg"},
+      {"id": 3, "name": "Пастила", "price": 1099, "weight": 400, 
+       "imageUrl": "/images/products/pastila.jpg"},
+      {"id": 4, "name": "Пончик с повидлом", "price": 1560, "weight": 400, 
+       "imageUrl": "/images/products/doughnut.jpg"},
+      {"id": 5, "name": "Эклер Вкусный", "price": 1500, "weight": 700, 
+       "imageUrl": "/images/products/eclair.jpg"},
+      {"id": 6, "name": "Бельгийские фавли", "price": 1099, "weight": 800, 
+       "imageUrl": "/images/products/waffles.jpg"},
+      {"id": 7, "name": "Суфле Суфле", "price": 1500, "weight": 500, 
+       "imageUrl": "/images/products/sufle.jpg"},
+      {"id": 8, "name": "Пирожное Наслаждение", "price": 1500, "weight": 400, 
+       "imageUrl": "/images/products/pirozhnoe.jpg"},
+      {"id": 9, "name": "Мармелад Домашний", "price": 1199, "weight": 700, 
+       "imageUrl": "/images/products/marmelad.jpg"},
+      {"id": 10, "name": "Халва", "price": 1199, "weight": 600, 
+       "imageUrl": "/images/products/halva.jpg"}
     ]
   };
   $scope.setOrdered = function (product_id) {
