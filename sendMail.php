674e4048452c55ced5dd504aa1ade9bd941bb9a5
Denis Shtabnoy <tr0ythex@gmail.com>
2016-03-25 09:42:58 +0000

    Adds message sending for callback form

--- sendMail.php
+++ sendMail.php
@@ -14,10 +14,10 @@
   $admin_email = "scytherclaw@gmail.com";
   $customer_email = $email;
   $subject = "Новый заказ";
-  $headers .= "From: Tomatort Info <info@tomatort.ru>";
   // Для отправки HTML-письма должен быть установлен заголовок Content-type
   $headers  = 'MIME-Version: 1.0' . "\r\n";
   $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+  $headers .= "From: Tomatort Info <info@tomatort.ru>";
   
   if ($cartData = $request->cartData) {
     $address = $request->address;
